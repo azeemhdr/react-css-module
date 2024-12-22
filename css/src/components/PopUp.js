45c8bf00b587cfd526ae45ea96b37b415d3dc9db
azeemhdr <comazeem@gmail.com>
2024-12-22 13:55:11 +0530

    pop-up complete

--- css/src/components/PopUp.js
+++ css/src/components/PopUp.js
@@ -1,16 +1,25 @@
-import React from 'react'
-import styles from './Button.module.css'
-import style from './PopUp.module.css'
-export default function PopUp({content,isPopUp,setIsPopUp}) {
+import React from "react";
+import styles from "./Button.module.css";
+import style from "./PopUp.module.css";
+import stylex from './Modal.module.css'
+export default function PopUp({ content, isPopUp, setIsPopUp }) {
   return (
-	<div>
-		<div className={`${styles.container}`}>
-			<div className={`${style.popUp}`}>
-				<h2>{content}</h2>
-				<button className={style.secondary} onClick={()=>{setIsPopUp(false)}}>close</button>
-			</div>
-
-		</div>
-	</div>
-  )
+    <div>
+      <div className={`${stylex.modal}`}>
+        <div className={`${styles.container}`}>
+          <div className={`${style.popUp}`}>
+            <h2>{content}</h2>
+            <button
+              className={styles.secondary}
+              onClick={() => {
+                setIsPopUp(false);
+              }}
+            >
+				Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
